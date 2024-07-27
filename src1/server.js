0cb3837921c661f51b7b689aca956c19d3b00b9a
Arden Weed <ardenjweed@gmail.com>
2024-07-26 20:09:03 -0700

    Moved MySQL test querying from recommendation to the search bar

--- src1/server.js
+++ src1/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 // Create an Express application
 const app = express();
@@ -26,16 +28,44 @@ db.connect(err => {
 // Serve static files (e.g., index.html)
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/recommendation', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'recommendation.html'));
 });
 
 // Handle /recommendation route
-app.get('/api/recommendation', (req, res) => {
-    console.log('Received request for /recommendation');
-    const queryString = `SELECT name FROM anime_filtered ORDER BY Popularity LIMIT 20`;
+// app.get('/api/recommendation', (req, res) => {
+//     console.log('Received request for /recommendation');
+//     const queryString = `SELECT name FROM anime_filtered ORDER BY Popularity LIMIT 20`;
 
-    db.query(queryString, (err, results) => {
+//     db.query(queryString, (err, results) => {
+//         if (err) {
+//             console.error('Error executing query:', err);
+//             res.status(500).send('500 - Internal Server Error');
+//             return;
+//         }
+
+//         // Print results to the terminal
+//         console.log('Query Results:', results);
+
+//         res.json(results);
+//     });
+// });
+
+app.get('/test', (req, res) => {
+    console.log('Received request for /test');
+    res.send('Test route is working!');
+});
+
+app.post('/submit', (req, res) => {
+    console.log("Hey")
+    const searchQuery = req.body.search;
+
+    const query = 'SELECT name FROM anime_filtered WHERE name LIKE ? LIMIT 20';
+    const formattedQuery = mysql.format(query, [`%${searchQuery}%`]);
+
+    db.query(formattedQuery, (err, results) => {
         if (err) {
             console.error('Error executing query:', err);
             res.status(500).send('500 - Internal Server Error');
@@ -44,20 +74,17 @@ app.get('/api/recommendation', (req, res) => {
 
         // Print results to the terminal
         console.log('Query Results:', results);
-
-        res.json(results);
     });
-});
-
-app.get('/test', (req, res) => {
-    console.log('Received request for /test');
-    res.send('Test route is working!');
-});
+    res.send('Search query saved successfully!');
+    });
+//});
 
 // Handle all other routes
 app.use((req, res) => {
     res.status(404).send('404 - Not Found');
 });
+
+
 
 // Start the server
 app.listen(port, () => {
